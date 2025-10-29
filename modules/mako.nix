3e8f60a1383c986e0b85b98b0acb9edbeec6e31f
Nova-archx86 <moorcode66@yahoo.com>
2025-10-29 07:44:57 +0000

    updated timeout

--- modules/mako.nix
+++ modules/mako.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       actions = true;
-      default-timeout = 15;
+      default-timeout = 15000;
       ignore-timeout = false;
       background-color = "#3b4252";
       border-color = "#5e81ac";
